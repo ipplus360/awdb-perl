11876a02736a4ecf22b21a34ef2915be32c0d748
libei <libei@ipplus360.com>
2020-11-02 18:29:02 +0800

    文件更新

--- test.pl
+++ test.pl
@@ -5,8 +5,13 @@ use feature qw( say );
 use Data::Printer;
 use AW::DB::Reader;
 use Net::Works::Address;
+use Encode;
+use JSON;
+use Data::Dumper;
 
 
-my $reader = AW::DB::Reader->new( file => './IP_city_single_WGS84_awdb.awdb' );
+my $reader = AW::DB::Reader->new( file => '../IP_city_single_BD09_WGS84_ipv6_awdb.awdb' );
 my $record = $reader->record_for_address('166.111.4.100');
+say @$record{'areacode'};
+say @$record{'continent'};
 say np $record;
